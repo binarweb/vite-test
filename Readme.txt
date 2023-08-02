ca4d2727ee0507eb162ea304752c2db4db3c9835
binarweb <38425768+binarweb@users.noreply.github.com>
2023-08-02 17:53:16 +0300

    wip

---
+++ Readme.txt
@@ -0,0 +1,4 @@
+https://dev.to/marcinwosinek/vite-in-a-simple-html-js-usecase-35k2
+https://getbootstrap.com/docs/5.2/getting-started/vite/
+https://tailwindcss.com/docs/guides/vite
+https://stackoverflow.com/a/66877705/9618184
---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+import '../scss/styles.scss'
+
+// Import the modules
+import "../contact-list/contact-list";
+
+// Import only the Bootstrap components we need
+// import { Popover } from 'bootstrap';
+import * as bootstrap from 'bootstrap'
+
+// Create an example popover
+// document.querySelectorAll('[data-bs-toggle="popover"]')
+//   .forEach(popover => {
+//     new Popover(popover)
+//   })
